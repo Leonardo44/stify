4d8b4524e1bc857f3a30f67d98e25a445ac82817
leonarrdo404 <leo@ese.plus>
2021-10-14 23:04:03 -0600

    SearchBar view

--- stify/Ui/Search/View/Search.swift
+++ stify/Ui/Search/View/Search.swift
@@ -134,7 +134,7 @@ struct Search: View {
                                 Spacer()
                             }
                         }
-                        Spacer()
+                        .background(Color.red)
                     }
                 }
             }
